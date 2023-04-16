60110466d75fe214a0171427c07b19bcef4ed188
Matthijs Besselink <matthijsbesselink@live.nl>
2023-04-16 09:43:53 +0200

    moved attachment/initialization of effect handler to storage_handle initialization.

--- lib/rp2040/src/GPIO/user_IO.cpp
+++ lib/rp2040/src/GPIO/user_IO.cpp
@@ -4,7 +4,13 @@
 #include <iostream>
 #include <new>
 
-std::weak_ptr<user_IO> user_IO::storage_handle = std::weak_ptr<user_IO>();
+std::weak_ptr<user_IO> initialize()
+{
+  std::clog << "initialize called.\n";
+  return std::weak_ptr<user_IO>();
+}
+
+std::weak_ptr<user_IO> user_IO::storage_handle = USE_SIMULATED_REGISTERS ? initialize() : std::weak_ptr<user_IO>();
 
 user_IO::user_IO() {
   // TODO: reset registers to enabled but cleared state.
