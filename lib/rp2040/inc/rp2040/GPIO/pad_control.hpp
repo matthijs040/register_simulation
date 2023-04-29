c6bb0f341831bf795267fec0a1d281d43dced0d5
Matthijs Besselink <matthijsbesselink@live.nl>
2023-04-29 10:33:09 +0200

    made pad_control use register unions. added initializer to set multiple bits at once for initialization.

--- lib/rp2040/inc/rp2040/GPIO/pad_control.hpp
+++ lib/rp2040/inc/rp2040/GPIO/pad_control.hpp
@@ -4,6 +4,7 @@
 #include <HAL/simulated_peripheral.hpp>
 #include <memory>
 #include <type_traits>
+#include "registers/pad_GPIO.hpp"
 
 class pad_control : std::conditional<USE_SIMULATED_REGISTERS,
                                      simulated_peripheral<pad_control>, void> {
@@ -20,39 +21,39 @@ private:
   static inline std::weak_ptr<pad_control> storage_handle;
   friend std::shared_ptr<pad_control> std::make_shared<pad_control>();
 
-  register_integral VOLTAGE_SELECT; // Voltage select. Per bank control
-  register_integral GPIO0;          // Pad control register
-  register_integral GPIO1;          // Pad control register
-  register_integral GPIO2;          // Pad control register
-  register_integral GPIO3;          // Pad control register
-  register_integral GPIO4;          // Pad control register
-  register_integral GPIO5;          // Pad control register
-  register_integral GPIO6;          // Pad control register
-  register_integral GPIO7;          // Pad control register
-  register_integral GPIO8;          // Pad control register
-  register_integral GPIO9;          // Pad control register
-  register_integral GPIO10;         // Pad control register
-  register_integral GPIO11;         // Pad control register
-  register_integral GPIO12;         // Pad control register
-  register_integral GPIO13;         // Pad control register
-  register_integral GPIO14;         // Pad control register
-  register_integral GPIO15;         // Pad control register
-  register_integral GPIO16;         // Pad control register
-  register_integral GPIO17;         // Pad control register
-  register_integral GPIO18;         // Pad control register
-  register_integral GPIO19;         // Pad control register
-  register_integral GPIO20;         // Pad control register
-  register_integral GPIO21;         // Pad control register
-  register_integral GPIO22;         // Pad control register
-  register_integral GPIO23;         // Pad control register
-  register_integral GPIO24;         // Pad control register
-  register_integral GPIO25;         // Pad control register
-  register_integral GPIO26;         // Pad control register
-  register_integral GPIO27;         // Pad control register
-  register_integral GPIO28;         // Pad control register
-  register_integral GPIO29;         // Pad control register
-  register_integral SWCLK;          // Pad control register
-  register_integral SWD;            // Pad control register
+  reg::VOLTAGE_SELECT VOLTAGE_SELECT; // Voltage select. Per bank control
+  reg::GPIO GPIO0;          // Pad control register
+  reg::GPIO GPIO1;          // Pad control register
+  reg::GPIO GPIO2;          // Pad control register
+  reg::GPIO GPIO3;          // Pad control register
+  reg::GPIO GPIO4;          // Pad control register
+  reg::GPIO GPIO5;          // Pad control register
+  reg::GPIO GPIO6;          // Pad control register
+  reg::GPIO GPIO7;          // Pad control register
+  reg::GPIO GPIO8;          // Pad control register
+  reg::GPIO GPIO9;          // Pad control register
+  reg::GPIO GPIO10;         // Pad control register
+  reg::GPIO GPIO11;         // Pad control register
+  reg::GPIO GPIO12;         // Pad control register
+  reg::GPIO GPIO13;         // Pad control register
+  reg::GPIO GPIO14;         // Pad control register
+  reg::GPIO GPIO15;         // Pad control register
+  reg::GPIO GPIO16;         // Pad control register
+  reg::GPIO GPIO17;         // Pad control register
+  reg::GPIO GPIO18;         // Pad control register
+  reg::GPIO GPIO19;         // Pad control register
+  reg::GPIO GPIO20;         // Pad control register
+  reg::GPIO GPIO21;         // Pad control register
+  reg::GPIO GPIO22;         // Pad control register
+  reg::GPIO GPIO23;         // Pad control register
+  reg::GPIO GPIO24;         // Pad control register
+  reg::GPIO GPIO25;         // Pad control register
+  reg::GPIO GPIO26;         // Pad control register
+  reg::GPIO GPIO27;         // Pad control register
+  reg::GPIO GPIO28;         // Pad control register
+  reg::GPIO GPIO29;         // Pad control register
+  reg::GPIO SWCLK;          // Pad control register
+  reg::GPIO SWD;            // Pad control register
 };
 
 template <> inline std::shared_ptr<pad_control> std::make_shared<pad_control>() {
--- lib/rp2040/inc/rp2040/GPIO/registers/pad_GPIO.hpp
+++ lib/rp2040/inc/rp2040/GPIO/registers/pad_GPIO.hpp
@@ -1,10 +1,18 @@
 #pragma once
-#include <HAL/bitfield.hpp>
 #include "shared_types.hpp"
+#include <HAL/bitfield.hpp>
 
 namespace reg {
+
+union VOLTAGE_SELECT {
+
+  enum class VOLTAGE_SELECT_states { volts_3_3, volts_1_8 };
+  VOLTAGE_SELECT(VOLTAGE_SELECT_states state) : voltage_select(state) {}
+  bitfield<VOLTAGE_SELECT_states, 0, 1> voltage_select;
+};
+
 union GPIO {
-  GPIO() {}
+  GPIO(register_integral initial_values) : full_register(initial_values) {}
   bitfield<reg::state, 7, 1> OD;
   bitfield<reg::state, 6, 1> IE;
   bitfield<reg::state, 4, 2> DRIVE;
@@ -12,6 +20,8 @@ union GPIO {
   bitfield<reg::state, 2, 1> PDE;
   bitfield<reg::state, 1, 1> SCHMITT;
   bitfield<reg::state, 0, 1> SLEWFAST;
+private:
+  register_integral full_register;
 };
 
 } // namespace reg
