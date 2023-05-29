7b60aec1f0b8e3626411ed6f4e822d701988ebbe
Matthijs Besselink <matthijsbesselink@live.nl>
2023-05-29 15:00:53 +0200

    got a release build blink working. The sleep seems to work without a debugger attached.

--- src/main.cpp
+++ src/main.cpp
@@ -8,28 +8,21 @@ int main(int argc, char const *argv[]) {
   const auto LED_pin = PICO_DEFAULT_LED_PIN;
 
   auto instance = GPIO(LED_pin);
-  std::cout << "initialization-result was: " << instance.initialization_result
-            << "\n";
-
   if (instance.initialization_result)
     return EXIT_FAILURE;
 
   auto mode = instance.get_pin_mode();
-  std::cout << "pre-GPIO_init() mode: " << std::to_underlying(mode) << "\n";
-  gpio_init(LED_pin);
-  std::cout << "post-GPIO_init() mode: " << std::to_underlying(mode) << "\n";
-  gpio_set_dir(LED_pin, GPIO_OUT);
-  mode = instance.get_pin_mode();
+  auto error = instance.set_pin_mode(GPIO::mode::output_only);
+  if (error)
+    return EXIT_FAILURE;
 
   while (true) {
-    gpio_put(LED_pin, true);
-    std::cout << "high: " << std::to_underlying(instance.get_pin_state())
-              << "\n";
-    sleep_ms(10);
-    gpio_put(LED_pin, false);
-    std::cout << "low: " << std::to_underlying(instance.get_pin_state())
-              << "\n";
-    sleep_ms(10);
+    auto error = instance.set_pin_state(
+        instance.get_pin_state() == GPIO::state::high ? GPIO::state::low
+                                                      : GPIO::state::high);
+    if (error)
+      return EXIT_FAILURE;
+    sleep_ms(1000);
   }
   return EXIT_SUCCESS;
 }
