f503b8d2fb14513495df2fe88b819e45952e6884
Matthijs Besselink <matthijsbesselink@live.nl>
2023-06-07 22:24:57 +0200

    Added first register.

--- lib/rp2040/inc/rp2040/time/clocks.hpp
+++ lib/rp2040/inc/rp2040/time/clocks.hpp
@@ -1,21 +1,23 @@
 #pragma once
 
+#include "registers/GP_clocks.hpp"
 #include <HAL/device_register.hpp>
 #include <HAL/simulated_peripheral.hpp>
 #include <type_traits>
 
 class clocks : std::conditional<USE_SIMULATED_REGISTERS,
-                                     simulated_peripheral<clocks>, void> {
+                                simulated_peripheral<clocks>, void> {
 public:
   static constexpr uintptr_t base_address = 0x40008000;
 
-  static clocks& get() noexcept;
+  static clocks &get() noexcept;
 
   ~clocks();
   void operator delete(void *addr);
 
+  reg::CLK_GPOUT0_CTRL CLK_GPOUT0_CTRL;
+
 private:
   clocks();
   void *operator new(std::size_t);
-
 };
--- lib/rp2040/inc/rp2040/time/registers/GP_clocks.hpp
+++ lib/rp2040/inc/rp2040/time/registers/GP_clocks.hpp
@@ -1,8 +1,30 @@
 #pragma once
 
 #include <HAL/bitfield.hpp>
+#include <rp2040/shared_types.hpp>
 
-union CLK_GPOUT0_CTRL
-{
-    
+namespace reg {
+union CLK_GPOUT0_CTRL {
+  bitfield<reg::state, 20, 1> NUDGE;
+  bitfield<register_integral, 16, 2> PHASE;
+  bitfield<reg::state, 12, 1> DC50;
+  bitfield<reg::state, 11, 1> ENABLE;
+  bitfield<reg::state, 10, 1> KILL;
+
+  enum class AUXSRC_states : register_integral {
+    clksrc_pll_sys = 0x0,
+    clksrc_gpin0 = 0x1,
+    clksrc_gpin1 = 0x2,
+    clksrc_pll_usb = 0x3,
+    rosc_clksrc = 0x4,
+    xosc_clksrc = 0x5,
+    clk_sys = 0x6,
+    clk_usb = 0x7,
+    clk_adc = 0x8,
+    clk_rtc = 0x9,
+    clk_ref = 0xA,
+  };
+  bitfield<AUXSRC_states, 5, 4> AUXSRC;
 };
+
+} // namespace reg
