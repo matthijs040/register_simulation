d42a9ea78d28e333272d691fea9c5a60c09e8ff4
Matthijs Besselink <matthijsbesselink@live.nl>
2023-10-07 17:12:45 +0200

    Upped the frequency in the flash script. Extended main with getter function and error printing.

--- push_binary_openocd.sh
+++ push_binary_openocd.sh
@@ -1,5 +1,5 @@
 openocd \
 -f interface/ftdi/ft232h-module-swd.cfg \
 -f target/rp2040.cfg \
--c "adapter speed 64" \
+-c "adapter speed 500" \
 -c "program build_app/main.elf verify reset exit" \
--- src/main.cpp
+++ src/main.cpp
@@ -7,15 +7,34 @@
 #include <cstring>
 #include <functional>
 #include <variant>
+#include <chrono>
 
 constexpr uint8_t LED_pin = PICO_DEFAULT_LED_PIN;
 
 void enable_LED_pin()
 {
   auto instance = GPIO(LED_pin);
-  instance.set_pin_mode(GPIO::mode::output_only);
-  instance.set_pin_state(GPIO::state::high);
-  std::cout << "Set LED high.\n";
+  if (instance.initialization_result)
+  {
+    std::cout << "initializing the LED failed with error: " << instance.initialization_result.message() << '\n';
+    return;
+  }
+
+  auto error = instance.set_pin_mode(GPIO::mode::output_only);
+  if (error)
+  {
+    std::cout << "Setting pin mode to output failed with error: " << error.message() << '\n';
+    return;
+  }
+  error = instance.set_pin_state(GPIO::state::high);
+  if (error)
+  {
+    std::cout << "Setting the pin state to high failed with error: " << error.message() << '\n';
+    return;
+  }
+
+  const char *state = instance.get_pin_state() == GPIO::state::high ? "high" : "low";
+  std::cout << "LED state is now: " << state << "\n";
 }
 
 void disable_LED_pin()
@@ -26,11 +45,36 @@ void disable_LED_pin()
   std::cout << "Set LED low.\n";
 }
 
+void get_LED_state()
+{
+  auto instance = GPIO(LED_pin);
+  const char *state = instance.get_pin_state() == GPIO::state::high ? "high" : "low";
+  std::cout << "LED state is: " << state << '\n';
+}
+
 using named_function = std::pair<const char *, std::function<void(void)>>;
 
 static const std::array functions = {
     named_function{"LED.set.high", std::bind(enable_LED_pin)},
-    named_function{"LED.set.low", std::bind(disable_LED_pin)}};
+    named_function{"LED.set.low", std::bind(disable_LED_pin)},
+    named_function{"LED.get", std::bind(get_LED_state)}};
+
+void get_string(std::span<char> data)
+{
+  std::size_t index = 0;
+  while (index < data.size())
+  {
+    int character = 0;
+    while ((character = getchar_timeout_us(1)) == PICO_ERROR_TIMEOUT)
+      sleep_ms(1);
+    std::cout << static_cast<char>(character);
+    std::cout.flush();
+
+    if (character == '\n' || character == '\r')
+      break;
+    data[index++] = character & 0xFF;
+  }
+}
 
 int main(int, char const *[])
 {
@@ -40,18 +84,18 @@ int main(int, char const *[])
   while (true)
   {
     constexpr size_t max_command_size = 30;
-    char read_line[max_command_size] = {};
+    char storage[max_command_size] = {};
+    std::memset(storage, '\0', sizeof(storage));
 
-    std::cout << "Enter command.\n";
-    if (std::cin.peek() == std::char_traits<char>::eof())
-    {
-      sleep_ms(1000);
-      continue;
-    }
-    std::cin.getline(read_line, max_command_size - 1);
+    std::cout << "Enter command." << std::endl;
+    std::span<char> line = {storage, sizeof(storage)};
+    get_string(line.subspan(0, line.size() - 1));
+    line.back() = '\0';
 
-    const auto found = std::find_if(functions.begin(), functions.end(), [read_line](named_function fn)
-                                    { return std::strlen(read_line) == std::strlen(fn.first) && std::strcmp(read_line, fn.first) == 0; });
+    std::cout << "gotten line: '" << line.data() << "'" << std::endl;
+
+    const auto found = std::find_if(functions.begin(), functions.end(), [line](named_function fn)
+                                    { return std::strlen(line.data()) == std::strlen(fn.first) && std::strcmp(line.data(), fn.first) == 0; });
 
     if (found != functions.end())
       found->second();
