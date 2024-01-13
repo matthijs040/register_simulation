7ad2fa7031606f7009804466b561a6a3290b31ed
Matthijs Besselink <matthijsbesselink@live.nl>
2024-01-13 13:16:33 +0100

    Added some more tests on the SPI initialization procedure.

--- tests/SPI_tests.cpp
+++ tests/SPI_tests.cpp
@@ -10,8 +10,29 @@ TEST(SPI_tests, SPI_handle_with_valid_configuration_will_initialize) {
   auto periph = SPI_peripheral::get(SPI_peripheral::ID::first);
   auto handle =
       rp2040_SPI(default_pins, SPI::mode::Motorola, SPI::role::main, true);
-  EXPECT_EQ(handle.initialization_result, error::standard_value::success);
-  if (handle.initialization_result) {
-    std::printf("Default SPI initialization failed with error: '%s'\n", handle.initialization_result.message());
-  }
+  ASSERT_EQ(handle.initialization_result, error::standard_value::success);
+}
+
+TEST(SPI_tests, SPI_handle_with_invalid_pins_will_error) {
+  auto periph = SPI_peripheral::get(SPI_peripheral::ID::first);
+  // The pins here are invalid as the TX and RX pins are swapped.
+  const auto invalid_pins = SPI::pins{3, 0, 1, 2};
+
+  auto handle =
+      rp2040_SPI(invalid_pins, SPI::mode::Motorola, SPI::role::main, true);
+  ASSERT_EQ(handle.initialization_result,
+            error::standard_value::invalid_argument);
+}
+
+TEST(SPI_tests, SPI_handle_will_not_initialize_if_already_active) {
+  auto periph = SPI_peripheral::get(SPI_peripheral::ID::first);
+
+  auto first_handle =
+      rp2040_SPI(default_pins, SPI::mode::Motorola, SPI::role::main, true);
+  ASSERT_EQ(first_handle.initialization_result, error::standard_value::success);
+
+  auto second_handle =
+      rp2040_SPI(default_pins, SPI::mode::Motorola, SPI::role::main, true);
+  ASSERT_EQ(second_handle.initialization_result,
+            error::standard_value::resource_unavailable_try_again);
 }
