5d1bc9ba3e48244522e390932aca5624ec40df8f
Matthijs Besselink <matthijsbesselink@live.nl>
2024-03-13 08:44:27 +0100

    Changed launch.json to not use cortex-debug. Removed LTO again from final application build. Now I can debug the UART snippet code.

--- push_binary_openocd.sh
+++ push_binary_openocd.sh
@@ -1,5 +1,6 @@
 openocd \
 -f interface/ftdi/ft232h-module-swd.cfg \
 -f target/rp2040.cfg \
--c "adapter speed 500" \
+-c "adapter speed 256" \
 -c "program build_app/UART_snippet.elf verify reset exit" \
+# -c "program ${HOME}/Downloads/picoprobe.elf verify reset exit" \
